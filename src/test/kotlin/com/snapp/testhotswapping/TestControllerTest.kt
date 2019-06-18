f82032d444ee3250774bd7c013fa0b379ef2a926
jc <sergicww@gmail.com>
2019-06-18 18:41:08 +0300

    server running with tests, todo: headers

--- src/test/kotlin/com/snapp/testhotswapping/TestControllerTest.kt
+++ src/test/kotlin/com/snapp/testhotswapping/TestControllerTest.kt
@@ -1,21 +1,34 @@
 package com.snapp.testhotswapping
 
-import com.snapp.testhotswapping.controller.TestController
+import io.restassured.RestAssured
+import io.restassured.RestAssured.given
 import io.restassured.http.Header
 import org.junit.Test
-import io.restassured.module.mockmvc.RestAssuredMockMvc.*
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.boot.web.server.LocalServerPort
+//import io.restassured.module.mockmvc.RestAssuredMockMvc.*
 
+
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TestControllerTest {
+
+    @LocalServerPort
+    lateinit var port: Integer
 
     @Test
     fun testThreadFilteHasDifferentIdInDifferentRequests() {
         val vars = listOf(1, 2, 3)
 
+        RestAssured.port = port.toInt()
+
         vars.forEach {
             given()
-                    .standaloneSetup(TestController())
-                    .header(Header("TestHeader${it}", "someData"))
-                    .param("name", "Johan")
+//                    .standaloneSetup(TestController())
+                    .header("TestHeader${it}", "someData")
                     .`when`()
                     .get("/a")
                     .then()
